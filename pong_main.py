c3720ada8daef7e1a6a06b655bee460a19633be9
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-10-02 12:02:31 +0100

    bad commit

--- pong_main.py
+++ pong_main.py
@@ -1,5 +1,4 @@
 from turtle import Screen, Turtle
-from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
@@ -7,15 +6,25 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
+paddle = Turtle()
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.shape("square")
+paddle.color("white")
+paddle.penup()
+paddle.goto(350, 0)
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
 
 screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "a")
-screen.onkey(l_paddle.go_down, "x")
-
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
 
 game_is_on = True
 
