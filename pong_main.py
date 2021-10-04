5ae6294e3a19d16f6352356c32e7e02de1a7bb6a
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-10-04 21:21:42 +0100

    Fix pep 8 styling issues

--- pong_main.py
+++ pong_main.py
@@ -27,26 +27,24 @@ while game_is_on:
     time.sleep(ball.move_speed)
     screen.update()
     ball.move()
-    
+
     # Detect collision with walls
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
-        
+
     # Detect collision with paddles
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or \
+            ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
-        
+
     # Detect when R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
         scoreboard.l_point()
-        
+
     # Detect when L paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
         scoreboard.r_point()
-    
-
-
 
 screen.exitonclick()
