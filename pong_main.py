49673213765b3c5de944a2624fc68f5f85f45363
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-10-04 16:56:17 +0100

    Add bounce function

--- pong_main.py
+++ pong_main.py
@@ -25,6 +25,12 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move()
+    
+    # Detecting collision with walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+        
+    
 
 
 
