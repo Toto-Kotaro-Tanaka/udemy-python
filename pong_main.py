dafbc1bf94e6e517f26bdddbb32060cba8468364
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-10-04 17:23:29 +0100

    Add detect paddles miss method

--- pong_main.py
+++ pong_main.py
@@ -30,9 +30,18 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
         
-    # Detect collision with r_paddle
+    # Detect collision with paddles
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+        
+    
+    # Detect when R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        
+    # Detect when L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
     
 
 
